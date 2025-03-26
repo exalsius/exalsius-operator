356ff51bc208645a3bff1ae503229380692ef5bd
srnbckr <soeren.becker@logsight.ai>
2025-03-26 04:12:10 +0100

    fix: fix relative paths

--- hack/start-dev-env.sh
+++ hack/start-dev-env.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # This creates a kind cluster with the config in kind-config.yaml
 # The docker socket is mounted to enable Docker-based cluster-api providers and a NodePort Service for Skypilot API
 # is exposed on port 30050
-kind create cluster --config kind-config.yaml
+kind create cluster --config "${SCRIPT_DIR}/kind-config.yaml"
